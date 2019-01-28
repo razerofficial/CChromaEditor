e76a7fda812a5a938485c2ecac7d289ffa6c7691
tgraupmann@gmail.com <tgraupmann@gmail.com>
2019-01-28 14:20:45 -0800

    Moved unit tests to a new class

---
+++ CConsoleEditor/UnitTests.h
@@ -0,0 +1,65 @@
+#pragma once
+
+class UnitTests
+{
+public:
+
+	static void IsPlaying(const char* name);
+
+	static void UnitTestsInit();
+
+	static void UnitTestsUninit();
+
+	static void UnitTestsPlayComposite();
+
+	static void UnitTestsOpenDialog();
+
+	static void UnitTestsOpenClose();
+
+	static void UnitTestsLayering();
+
+	static void UnitTestsLoadedAnimations();
+
+	static void UnitTestsSetKeys();
+
+	static void UnitTestsClear();
+
+	static void UnitTestsWait();
+
+	static void UnitTestsCopy();
+
+	static void UnitTestsMisc();
+
+	static void UnitTestsOffset();
+
+	static void UnitTestsNonZero();
+
+	static void UnitTestsHDKIndividualLEDs();
+
+	static void UnitTestsHDKIndividualLEDsGradient();
+
+	static void UnitTestsCreateAnimation();
+
+	static void UnitTestsKeyboardCustom();
+
+	static void UnitTestsFadeStart();
+
+	static void UnitTestsFadeEnd();
+
+	static void UnitTestsCopyAnimation();
+
+	static void UnitTestsSaveAnimation();
+
+	static void UnitTestsSaveAnimation2();
+
+	static void UnitTestsCreateRandomBlackAndWhite();
+
+	static void UnitTestsDuplicateFirstFrame();
+
+	static void Run();
+
+	static int OpenAndPlay(const char* path);
+
+	static int CloseAnimation(int animationId);
+
+};
